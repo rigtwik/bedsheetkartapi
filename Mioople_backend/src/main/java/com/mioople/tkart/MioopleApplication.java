af710c4f558792f505974cd3731a3d02a7a7953b
Ritik Maheshwari <rigwik@hotmail.com>
2021-04-05 00:45:11 +0530

    first commit of bed sheet shopping app

---
+++ Mioople_backend/src/main/java/com/mioople/tkart/MioopleApplication.java
@@ -0,0 +1,13 @@
+package com.mioople.tkart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MioopleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MioopleApplication.class, args);
+	}
+
+}
---
+++ Mioople_backend/src/test/java/com/mioople/tkart/MioopleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mioople.tkart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MioopleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
